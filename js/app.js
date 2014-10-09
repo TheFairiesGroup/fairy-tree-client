5d24a392094b197a1743a5a286e64783a2da8db7
Maya Lekova <apokalyptra@gmail.com>
2014-10-09 18:13:37 +0300

    Added another rudimentary visualization using Reingold–Tilford Tree

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@
                     templateUrl: "partials/subjects.html",
                     controller: "SubjectsController"
                 })
+                .when("/major_rttree/:majorId", {
+                    templateUrl: "partials/subjects_rttree.html",
+                    controller: "SubjectsController"
+                })
                 .when("/about", {
                     templateUrl: "partials/about.html"
                 })
---
+++ js/d3/radial_rttree.js
@@ -0,0 +1,100 @@
+(function() {
+    angular.module('fairyTree.directives', ['d3', 'fairyTree.factories']).directive('radialRttree', ['$window', '$timeout', 'd3Service', '$u',
+        function($window, $timeout, d3Service, $u) {
+            return {
+                restrict: 'A',
+                scope: {
+                    courses: '=',
+                    major: '='
+                },
+                link: function($scope, element, attrs) {
+                    d3Service.d3().then(function(d3) {
+                        $scope.$watch('courses', function(courses) {
+                            if (!courses || !courses.length) { return; }
+
+                        var diameter = 960;
+
+                        var tree = d3.layout.tree()
+                            .size([360, diameter / 2 - 120])
+                            .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+                        var diagonal = d3.svg.diagonal.radial()
+                                        .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+                        var svg = d3.select("body").append("svg")
+                            .attr("width", diameter)
+                            .attr("height", diameter - 150)
+                          .append("g")
+                            .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+                            var findNodes = function(courses) {
+                                return courses.map(function(current) {
+                                    return {
+                                        name: current.display_name,
+                                        parent: 'root',
+                                        id: current.$id,
+                                        course: current
+                                    };
+                                });
+                            };
+
+                            var nodes = tree.nodes({name: $scope.major.display_name, children: findNodes(courses)});
+                            var links = $u.buildEdges(courses).map(function(edge) {
+                                var source, target;
+
+                                for (var i = 0; i < nodes.length; i++) {
+                                    if (edge.from.$id == nodes[i].id) {
+                                        source = nodes[i];
+                                    }
+                                    if (edge.to.$id == nodes[i].id) {
+                                        target = nodes[i];
+                                    }
+
+                                    if (source && target) { break; }
+                                };
+
+                                if (!(source && target)) { debugger; }
+
+                                return {
+                                    source: source,
+                                    target: target,
+                                };
+                            });
+
+                            var link = svg.selectAll(".link")
+                              .data(links)
+                            .enter().append("path")
+                              .attr("class", "link")
+                              .attr("d", diagonal);
+
+                            var node = svg.selectAll(".node")
+                              .data(nodes)
+                            .enter().append("g")
+                              .attr("class", "node")
+                              .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+
+                            node.append("circle")
+                              .attr("r", 4.5)
+                              .attr("fill", function(d) { return d.size ? "#00" + (d.size + 10050).toString(16) : "red"; });
+
+                            node.append("text")
+                              .attr("dy", ".31em")
+                              .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+                              .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+                              .text(function(d) { return d.name; });
+
+                            node.on('mouseover', function(d) {
+                                svg.selectAll(".link")
+                                    .filter(function(data) { return (data.source.id != d.id && data.target.id != d.id) })
+                                    .attr('stroke-opacity', 0.1);
+                            })
+                            .on('mouseout', function() { svg.selectAll('.link').attr('stroke-opacity', 1) });
+
+                            d3.select(self.frameElement).style("height", diameter + "px");
+                        });
+                    });
+                }
+            }
+        }
+    ])
+}());
